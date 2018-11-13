67a4db9da39d8832b71bb6e5bc3817f89709ec66
侯帅 <houshuai@houshuaidemacbook-pro.local>
2018-11-13 23:18:58 +0800

    init

---
+++ MyIphonePro/MyIphonePro/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MyIphonePro
+//
+//  Created by houshuai on 2018/11/11.
+//  Copyright © 2018年 houshuai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var showLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        NSLog("jessehou view load")
+        print("ok")
+        self.showLabel.text = "侯帅最喜欢谁?"
+        
+    }
+    
+    @IBAction func clickButton(_ sender: Any) {
+        self.showLabel.text = "官远芳"
+    }
+    
+}
+
--- MySwiftPro/MySwiftPro/ViewController.swift
+++ MySwiftPro/MySwiftPro/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  MySwiftPro
 //
-//  Created by houshuai on 2018/11/7.
+//  Created by houshuai on 2018/11/11.
 //  Copyright © 2018年 houshuai. All rights reserved.
 //
 
